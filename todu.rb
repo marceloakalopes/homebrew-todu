4e1c4089758bc8b4cb435a35014e001abbbef3f8
marceloakalopes <marcelo.lopes2001@hotmail.com>
2024-09-22 19:42:04 -0400

    v1.0.0

--- todu.rb
+++ todu.rb
@@ -1,19 +1,11 @@
 class Todu < Formula
-    desc "A simple to-do CLI app"
-    homepage "https://github.com/marceloakalopes/todu"
-    url "https://github.com/marceloakalopes/todu/releases/download/v0.1.0/todu-0.1.0.tar.gz"
-    sha256 "8618d539fa447e00459ae721801f20f9b513c5e07cf3255abd24f949ac208d40"
-    version "0.1.0"
-  
-    def install
-      bin.install "todu"  # Installs the binary in /usr/local/bin
-    end
-  
-    def caveats
-      <<~EOS
-        Thanks for installing Todu CLI. You can manage tasks with:
-        - todu list
-        - todu new
-      EOS
-    end
+  desc "Todu is a simple terminal-based to-do list manager"
+  homepage "https://github.com/marceloakalopes/todu"
+  url "https://github.com/marceloakalopes/todu/releases/download/v1.0.0/todu-1.0.0-macos.tar.gz"
+  sha256 "e54127009d4609eae07bb2f67e083bd3e50e1953135b0973d6548576a4284cb4`"
+  license "MIT"
+
+  def install
+    bin.install "todu"
   end
+end
