a1fdf8c2a925be0970c80da5c094908ee67efb1a
marceloakalopes <marcelo.lopes2001@hotmail.com>
2024-09-22 20:26:42 -0400

    hash update

--- todu.rb
+++ todu.rb
@@ -2,7 +2,7 @@ class Todu < Formula
   desc "Todu is a simple terminal-based to-do list manager"
   homepage "https://github.com/marceloakalopes/todu"
   url "https://github.com/marceloakalopes/todu/releases/download/v1.0.0/todu-1.0.0-macos.tar.gz"
-  sha256 "e54127009d4609eae07bb2f67e083bd3e50e1953135b0973d6548576a4284cb4`"
+  sha256 "e54127009d4609eae07bb2f67e083bd3e50e1953135b0973d6548576a4284cb4"
   license "MIT"
 
   def install
